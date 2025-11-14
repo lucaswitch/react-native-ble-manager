2a6fbd7b34505b659069bcce183ccf8c99938622
Lucas Alves <lucas.developing@gmail.com>
2025-11-14 18:00:28 -0300

    Conflict.

---
+++ example/app.config.js
@@ -0,0 +1,32 @@
+
+export default ({ config }) => {
+  if (process.env.MY_ENVIRONMENT === 'production') {
+    config.name = 'RN BLE Example';
+  } else {
+    config.name = 'RN BLE Example Dev';
+  }
+
+  return {
+    ...config,
+    android: {
+      package: 'it.innove.example.ble',
+    },
+    ios: {
+      bundleIdentifier: 'it.innove.example.ble',
+      infoPlist: {
+        NSAccessorySetupKitSupports: ['Bluetooth'],
+        NSAccessorySetupBluetoothServices: [
+          '0000180D-0000-1000-8000-00805F9B34FB',
+        ],
+        NSAccessorySetupBluetoothNames: ['Polar'],
+      },
+    },
+    plugins: [
+      [
+        '../app.plugin.js',
+        { companionDeviceEnabled: true, neverForLocation: true },
+      ],
+      'expo-build-properties',
+    ],
+  };
+};
